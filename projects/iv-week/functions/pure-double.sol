9975f9a177c93553107d94c9710575af9d1b1e44
bujosa <davidbujosa@gmail.com>
2023-01-25 21:39:38 -0400

    feat: add gitignore for autoformat code

--- projects/iv-week/functions/pure-double.sol
+++ projects/iv-week/functions/pure-double.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 contract Contract {
-    function double(uint x) external pure returns(uint sum) {
-    sum = 2 * x;
-}
+    function double(uint256 x) external pure returns (uint256 sum) {
+        sum = 2 * x;
+    }
 }
