0b8c48361b810e83a2dba3c89ce737fef180a71e
bujosa <davidbujosa@gmail.com>
2023-01-28 18:27:29 -0400

    fix(charity): add tip and private address

--- projects/iv-week/address-interactions/charity/Contract.sol
+++ projects/iv-week/address-interactions/charity/Contract.sol
@@ -3,15 +3,22 @@ pragma solidity ^0.8.4;
 
 contract Contract {
     address public owner;
+    address private charityAddr;
 
-    constructor() {
+    constructor(address _charityAddr) {
         owner = msg.sender;
+        charityAddr = _charityAddr;
+    }
+
+    function tip() public payable {
+        (bool success, ) = owner.call{value: msg.value}("");
+        require(success, "Transfer failed");
+    }
+
+    function donate() public {
+        (bool success, ) = charityAddr.call{value: address(this).balance}("");
+        require(success, "Transfer failed");
     }
 
     receive() external payable {}
-
-    function tip() public payable {
-        (bool s, ) = owner.call{value: msg.value}("");
-        require(s);
-    }
 }
