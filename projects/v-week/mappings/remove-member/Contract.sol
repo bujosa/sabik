30a85aed890a8140229df5f79095a9317e5fe5a8
bujosa <davidbujosa@gmail.com>
2023-01-29 03:34:52 -0400

    feat(v): mappings - removeMember

---
+++ projects/v-week/mappings/remove-member/Contract.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Contract {
+    mapping(address => bool) public members;
+
+    function addMember(address _member) public {
+        members[_member] = true;
+    }
+
+    function isMember(address _member) public view returns (bool) {
+        return members[_member];
+    }
+
+    function removeMember(address _member) public {
+        delete members[_member];
+    }
+}
