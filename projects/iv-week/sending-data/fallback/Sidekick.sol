13ce49af5e6ccbd9ce1a1b1094690223f73e785e
bujosa <davidbujosa@gmail.com>
2023-01-29 01:42:07 -0400

    fix(iv): call fallback

---
+++ projects/iv-week/sending-data/fallback/Sidekick.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Sidekick {
+    function makeContact(address hero) external {
+        (bool s, ) = hero.call("fallback");
+        require(s);
+    }
+}
