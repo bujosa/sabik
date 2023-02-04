634b05e46f6157a1662e14752e8ac4a8017d115a
bujosa <davidbujosa@gmail.com>
2023-02-04 04:27:57 -0400

    feat(v): add constructor

--- projects/v-week/escrow-contract/setup/Escrow.sol
+++ projects/v-week/escrow-contract/setup/Escrow.sol
@@ -5,4 +5,10 @@ contract Escrow {
     address public arbiter;
     address public depositor;
     address public beneficiary;
+
+    constructor(address _arbiter, address _beneficiary) payable {
+        arbiter = _arbiter;
+        depositor = msg.sender;
+        beneficiary = _beneficiary;
+    }
 }
