5db1255e6fd2a8cd67620920e5c753e5c9295fc2
bujosa <davidbujosa@gmail.com>
2023-02-04 04:30:19 -0400

    feat(v): add approve function

--- projects/v-week/escrow-contract/setup/Escrow.sol
+++ projects/v-week/escrow-contract/setup/Escrow.sol
@@ -5,10 +5,17 @@ contract Escrow {
     address public arbiter;
     address public depositor;
     address public beneficiary;
+    bool public isApproved = false;
 
     constructor(address _arbiter, address _beneficiary) payable {
         arbiter = _arbiter;
         depositor = msg.sender;
         beneficiary = _beneficiary;
+    }
+
+    function approve() external {
+        require(msg.sender == arbiter, "You are not the arbiter");
+        isApproved = true;
+        payable(beneficiary).transfer(address(this).balance);
     }
 }
