6e82c1442dd755bbfb4b1ff610145944e1b2d509
bujosa <davidbujosa@gmail.com>
2023-02-07 02:36:14 -0400

    feat(vi): create paybill function

--- projects/vi-week/solidity-challenges/party/Party.sol
+++ projects/vi-week/solidity-challenges/party/Party.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.4;
 
 contract Party {
     uint256 public amount;
+    uint256 public poolFunds;
     address[] public guests;
 
     constructor(uint256 _amount) {
@@ -18,6 +19,19 @@ contract Party {
 
     function rsvp() public payable oneGuestPerAddress {
         require(msg.value == amount, "You must pay the right amount");
+        poolFunds += msg.value;
         guests.push(msg.sender);
+    }
+
+    function payBill(address venue, uint256 totalCost) public {
+        require(poolFunds >= totalCost, "Not enough funds in the pool");
+        payable(venue).transfer(totalCost);
+        poolFunds -= totalCost;
+        uint256 amountPerGuest = poolFunds / guests.length;
+
+        for (uint256 i = 0; i < guests.length; i++) {
+            payable(guests[i]).transfer(amountPerGuest);
+            poolFunds -= amountPerGuest;
+        }
     }
 }
