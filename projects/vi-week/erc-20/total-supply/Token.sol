f7bf5a2cbc17bbf44d8cb5934cee2b14009b161a
bujosa <davidbujosa@gmail.com>
2023-02-12 23:00:02 -0400

    feat(vi): add balanceOf util

--- projects/vi-week/erc-20/total-supply/Token.sol
+++ projects/vi-week/erc-20/total-supply/Token.sol
@@ -6,4 +6,10 @@ contract Token {
     string public name = "Vi Week Token";
     string public symbol = "VWT";
     uint8 public decimals = 18;
+
+    mapping(address => uint256) private _balanceOf;
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return _balanceOf[_owner];
+    }
 }
