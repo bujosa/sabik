225ecda41db0a92f3b1681e521122383e74023b3
bujosa <davidbujosa@gmail.com>
2023-02-12 22:58:01 -0400

    feat(vi): add name, symbol and decimals fields

--- projects/vi-week/erc-20/total-supply/Token.sol
+++ projects/vi-week/erc-20/total-supply/Token.sol
@@ -3,4 +3,7 @@ pragma solidity ^0.8.4;
 
 contract Token {
     uint256 public totalSupply;
+    string public name = "Vi Week Token";
+    string public symbol = "VWT";
+    uint8 public decimals = 18;
 }
--- projects/vi-week/erc-20/total-supply/test.js
+++ projects/vi-week/erc-20/total-supply/test.js
@@ -8,6 +8,29 @@ describe('Token', () => {
     await token.deployed();
   });
 
+  describe('ERC20 Optional', () => {
+    context('`name`', () => {
+      it('should return the correct name', async () => {
+        const name = await token.callStatic.name();
+        assert.isAtLeast(name.length, 1);
+      });
+    });
+
+    context('`symbol`', () => {
+      it('should return the correct name', async () => {
+        const sym = await token.callStatic.symbol();
+        assert.equal(sym.length, 3);
+      });
+    });
+
+    context('`decimals`', () => {
+      it('should return the correct name', async () => {
+        const decimals = await token.callStatic.decimals();
+        assert.equal(decimals, 18);
+      });
+    });
+  });
+
   describe('ERC20 Standard', () => {
     context('totalSupply', () => {
       it('should return zero', async () => {
