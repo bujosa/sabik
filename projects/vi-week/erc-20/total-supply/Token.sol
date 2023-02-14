d3f9e22412513776bba723369b003e3f46076f54
bujosa <davidbujosa@gmail.com>
2023-02-13 20:23:08 -0400

    feat(vi): add transfer

--- projects/vi-week/erc-20/total-supply/Token.sol
+++ projects/vi-week/erc-20/total-supply/Token.sol
@@ -17,4 +17,14 @@ contract Token {
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return _balanceOf[_owner];
     }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(_balanceOf[msg.sender] >= _value, "Insufficient balance");
+        _balanceOf[msg.sender] -= _value;
+        _balanceOf[_to] += _value;
+        return true;
+    }
 }
