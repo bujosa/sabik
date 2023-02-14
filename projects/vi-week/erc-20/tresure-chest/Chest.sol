dec1de4d81a23f308d5fabddb74a0d0439e96b1b
bujosa <davidbujosa@gmail.com>
2023-02-14 00:38:23 -0400

    feat: add example using IERC20

--- projects/vi-week/erc-20/tresure-chest/Chest.sol
+++ projects/vi-week/erc-20/tresure-chest/Chest.sol
@@ -3,4 +3,11 @@ pragma solidity ^0.8.4;
 
 import "./IERC20.sol";
 
-contract Chest {}
+contract Chest {
+    function plunder(address[] memory _tokens) external {
+        for (uint256 i = 0; i < _tokens.length; i++) {
+            IERC20 token = IERC20(_tokens[i]);
+            token.transfer(msg.sender, token.balanceOf(address(this)));
+        }
+    }
+}
