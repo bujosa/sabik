9a198e8bf7c24a4f8dd88db4d799a2e63c8997b9
bujosa <davidbujosa@gmail.com>
2023-02-23 20:58:15 -0400

    feat(vii): add prime game contract

--- projects/vii-week/libraries/evenly-divides/test.js
+++ projects/vii-week/libraries/evenly-divides/test.js
@@ -1,41 +1,76 @@
 const { assert } = require('chai');
+
+const mineBlock = () => ethers.provider.send('evm_mine');
+
 describe('Prime', function () {
-  let library;
+  let contract;
   before(async () => {
     const Prime = await ethers.getContractFactory('Prime');
     library = await Prime.deploy();
     await library.deployed();
+
+    const PrimeGame = await ethers.getContractFactory('PrimeGame', {
+      libraries: { Prime: library.address },
+    });
+    contract = await PrimeGame.deploy();
+    await contract.deployed();
   });
 
-  it('should detect numbers that evenly divide', async () => {
-    const pairs = [
-      [4, 2],
-      [16, 4],
-      [200, 50],
-    ];
-    for (let i = 0; i < pairs.length; i++) {
-      const [x, y] = pairs[i];
-      const dividesEvenly = await library.callStatic.dividesEvenly(x, y);
-      assert(
-        dividesEvenly,
-        `Expected dividesEvenly to return true for ${x} divided by ${y}!`
-      );
-    }
+  // blocks 0 & 1 are used for initialization
+  describe('for block number 3', () => {
+    before(async () => {
+      await mineBlock();
+    });
+
+    it('should be a winner', async () => {
+      const isWinner = await contract.callStatic.isWinner();
+      assert(isWinner, 'Expected block 3 to be a winner');
+    });
   });
 
-  it('should detect numbers that do not evenly divide', async () => {
-    const pairs = [
-      [5, 2],
-      [22, 4],
-      [199, 50],
-    ];
-    for (let i = 0; i < pairs.length; i++) {
-      const [x, y] = pairs[i];
-      const dividesEvenly = await library.callStatic.dividesEvenly(x, y);
-      assert(
-        !dividesEvenly,
-        `Expected dividesEvenly to return false for ${x} divided by ${y}!`
-      );
-    }
+  describe('for block number 4', () => {
+    before(async () => {
+      await mineBlock();
+    });
+
+    it('should be a winner', async () => {
+      const isWinner = await contract.callStatic.isWinner();
+      assert(!isWinner, 'Expected block 4 to not be a winner');
+    });
+  });
+
+  describe('for block number 5', () => {
+    before(async () => {
+      await mineBlock();
+    });
+
+    it('should be a winner', async () => {
+      const isWinner = await contract.callStatic.isWinner();
+      assert(isWinner, 'Expected block 5 to be a winner');
+    });
+  });
+
+  describe('for block number 18', () => {
+    before(async () => {
+      for (let i = 5; i < 18; i++) {
+        await mineBlock();
+      }
+    });
+
+    it('should be a winner', async () => {
+      const isWinner = await contract.callStatic.isWinner();
+      assert(!isWinner, 'Expected block 18 to not be a winner');
+    });
+  });
+
+  describe('for block number 19', () => {
+    before(async () => {
+      await mineBlock();
+    });
+
+    it('should be a winner', async () => {
+      const isWinner = await contract.callStatic.isWinner();
+      assert(isWinner, 'Expected block 19 to be a winner');
+    });
   });
 });
