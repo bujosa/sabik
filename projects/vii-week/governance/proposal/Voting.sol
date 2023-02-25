152ea741f5ccb6b3028a02e84ce141541771d1a8
bujosa <davidbujosa@gmail.com>
2023-02-25 02:53:16 -0400

    feat(vii): init proposal

---
+++ projects/vii-week/governance/proposal/Voting.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Voting {
+    struct Proposal {
+        address target;
+        bytes data;
+        uint yesCount;
+        uint noCount;
+    }
+    
+}
